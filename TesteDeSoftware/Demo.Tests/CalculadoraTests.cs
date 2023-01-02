dd4304c3d25c976a614afbc52c91b142ea402654
Ilola Nicolau João <ilola@keltecnologia.com.br>
2023-01-02 11:51:42 +0100

    Primeiros testes

---
+++ TesteDeSoftware/Demo.Tests/CalculadoraTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Calculadora_Somar_RetornarValorSoma()
+        {
+            //Arrange
+            var calculadora = new Calculadora();
+            //Act
+            var resultado = calculadora.Somar(4, 4);
+            //Assert
+            Assert.Equal(8, resultado);
+        }
+    }
+}
---
+++ TesteDeSoftware/Demo/Calculadora.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Calculadora
+    {
+        public double Somar(int v1, int v2) => v1 + v2;
+        public int Dividir(int v1, int v2)=>v2 / v1;
+    }
+}
